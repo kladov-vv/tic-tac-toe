04287ca4a3ec1027dfaa00e7f3490ab7eb04f37b
kladov-vv <kladov.vv@yandex.com>
2026-03-16 21:52:16 +1000

    Rewrite modules and factories with more usage of returning methods

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const board = (function() {
-    const coordinates = [ , , , , , , , , , ];    
-    return { coordinates };
+    let boardArray = [ , , , , , , , , , ];
+    
+    const get = () => boardArray;
+    const clear = () => boardArray = [ , , , , , , , , , ];
+    
+    const markPosition = (marker, index) => boardArray[index] = marker;
+
+    const getPlayerPositions = (marker) => {
+        let playerPositions = '';
+        boardArray.forEach((position, index) => {
+            if (position === marker) {
+                playerPositions = playerPositions.concat(index);
+            }
+        });
+        return playerPositions;
+    }
+    
+    return { get, clear, markPosition, getPlayerPositions };
 })();
 
 const createPlayer = (name, marker) => {
-    let positions = '';
-    return { name, marker, positions }
+    const getName = () => name;
+    const getMarker = () => marker;
+
+    const getPositions = () => board.getPlayerPositions(marker);
+
+    let score = 0;
+    const getScore = () => score;
+    const increaseScore = () => score++;
+    const clearScore = () => score = 0;
+
+    return { getName, getMarker, getPositions, getScore, increaseScore, clearScore }
 }
 
 const player1 = createPlayer('Player One', 'x');
@@ -14,35 +39,64 @@ const player2 = createPlayer('Player Two', 'o');
 const game = (function() {    
     let currentPlayer = player1;
 
+    const endRound = () => {
+        console.log(`${player1.getName()}: ${player1.getScore()} | ${player2.getName()}: ${player2.getScore()}`);
+        
+        board.clear();
+        currentPlayer = player1;
+    }
+
+    const endGame = () => {
+        endRound();
+        player1.clearScore;
+        player2.clearScore;
+    }
+
     const checkWinner = () => {
         const patterns = ['012', '345', '678', '036', '147', '258', '048', '246'];
 
-        // check if won
         patterns.forEach((pattern) => {
-            const regex = new RegExp(`${pattern.charAt(0)}.*${pattern.charAt(1)}.*${pattern.charAt(2)}`);
-            if (regex.test(currentPlayer.positions)) {
-                console.log(`${currentPlayer.name} won!`);
+            const regex = new RegExp(`(?=.*${pattern.charAt(0)})(?=.*${pattern.charAt(1)})(?=.*${pattern.charAt(2)}).*`);
+            
+            if (regex.test(currentPlayer.getPositions())) {
+                currentPlayer.increaseScore();
+                console.log(`${currentPlayer.getName()} won!`);
+                endRound();
             }
         });
+    }
 
-        // check if draw
-        if (!board.coordinates.includes(undefined)) {
+    const checkDraw = () => {
+        if (!board.get().includes(undefined)) {
             console.log('draw!');
+            endRound();
         }
     }
 
-    const changePlayer = () => {
+    const switchPlayer = () => {
         currentPlayer = (currentPlayer === player1) ? player2 : player1;
     }
 
-    const takeTurn = (index) => {
-        if (board.coordinates[index] === undefined) {
-            board.coordinates[index] = currentPlayer.marker;
-            currentPlayer.positions = currentPlayer.positions.concat(index);
+    const takeTurn = (position) => {
+        if (board.get()[position] === undefined) {
+            board.markPosition(currentPlayer.getMarker(), position);
+            // displayControl.updateDisplay();
+
+            console.log(currentPlayer.getName());
+            console.log(currentPlayer.getPositions());
+            
             checkWinner();
-            changePlayer();    
+            checkDraw();
+            switchPlayer();
+             
         }
     }
 
-    return { takeTurn };
+    return { takeTurn, endGame };
 })();
+
+game.takeTurn(4);
+game.takeTurn(6);
+game.takeTurn(0);
+game.takeTurn(7);
+game.takeTurn(8);
