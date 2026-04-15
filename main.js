d22d63cdaa7d13aa143d1cc7eb699566b51d26ff
kladov-vv <kladov.vv@yandex.com>
2026-04-15 12:46:26 +1000

    Complete project

--- main.js
+++ main.js
@@ -1,21 +1,119 @@
+const displayControl = (function() {
+    const dialog = document.querySelector('dialog');
+    const btnStart = document.querySelector('.btn-start');
+
+    const form = document.querySelector('form');
+    const btnCloseDialog = document.querySelector('.btn-close-dialog');
+
+    const gameBoard = document.querySelector('.game-board');
+    const playerContainers = document.querySelectorAll('.player-container');
+    
+    const statusBar = document.querySelector('.status-bar');
+
+    const player1DisplayedName = document.querySelector('.player-1 .player-name');
+    const player2DisplayedName = document.querySelector('.player-2 .player-name');
+
+    const player1DisplayedScore = document.querySelector('.player-1 .player-score');
+    const player2DisplayedScore = document.querySelector('.player-2 .player-score');
+
+
+    // dialog events
+    btnStart.addEventListener('click', () => {
+        dialog.showModal();
+    })
+
+    btnCloseDialog.addEventListener('click', () => {
+        form.reset();
+        dialog.close();
+    });
+
+    // start game
+    form.addEventListener('submit', () => {    
+        const formData = new FormData(form);
+        const formObject = Object.fromEntries(formData.entries());
+        const player1Name = formObject.player1Name;
+        const player2Name = formObject.player2Name;
+        
+        game.start(player1Name, player2Name);
+        
+        // changeVisibility(content);
+        changeVisibility(gameBoard);
+        changeVisibility(statusBar);
+        playerContainers.forEach((node) => changeVisibility(node));
+        
+         
+        displayNames(player1Name, player2Name);
+        
+        updateScreen();
+    });
+
+    gameBoard.addEventListener('click', (event) => {
+        if (event.target.id.at(-1).match(/^[0-8]{1}$/)) {
+            const position = event.target.id.at(-1);
+            game.takeTurn(position);
+            updateScreen();
+        } 
+    })
+    
+    // update information on screen
+    const updateScreen = () => {
+        let info = game.getTurnInfo();
+        
+        updateScore(info.score1, info.score2);
+        updateStatusBar(info.turnResult);
+        updateBoard();
+    }
+
+    const updateBoard = () => {
+        board.get().forEach((position, index) => {
+            const place = document.querySelector(`#place-${index}`);
+            place.textContent = position;
+        })
+    }
+
+    const updateStatusBar = (message) => {
+        statusBar.textContent = message;
+    }
+
+    const updateScore = (score1, score2) => {
+        player1DisplayedScore.textContent = `Score: ${score1}`;
+        player2DisplayedScore.textContent = `Score: ${score2}`;
+
+    }
+
+    const displayNames = (name1, name2) => {
+        player1DisplayedName.textContent = name1;
+        player2DisplayedName.textContent = name2;
+    }
+
+    const changeVisibility = (element) => {
+        if (element.style.visibility = 'hidden') {
+            element.style.visibility = 'visible';
+        }
+    }
+})();
+
 const board = (function() {
-    let boardArray = [ , , , , , , , , , ];
-    
+    const boardArray = new Array(9).fill('');
+
     const get = () => boardArray;
-    const clear = () => boardArray = [ , , , , , , , , , ];
-    
+    const clear = () => boardArray.fill('');
+
     const markPosition = (marker, index) => boardArray[index] = marker;
 
-    const getPlayerPositions = (marker) => {
+    const getPlayerPositions = (player) => {
+        const marker = player.getMarker();
         let playerPositions = '';
+        
         boardArray.forEach((position, index) => {
             if (position === marker) {
                 playerPositions = playerPositions.concat(index);
             }
         });
+        
         return playerPositions;
     }
-    
+
     return { get, clear, markPosition, getPlayerPositions };
 })();
 
@@ -23,80 +121,80 @@ const createPlayer = (name, marker) => {
     const getName = () => name;
     const getMarker = () => marker;
 
-    const getPositions = () => board.getPlayerPositions(marker);
-
     let score = 0;
     const getScore = () => score;
     const increaseScore = () => score++;
     const clearScore = () => score = 0;
 
-    return { getName, getMarker, getPositions, getScore, increaseScore, clearScore }
+    return { getName, getMarker, getScore, increaseScore, clearScore }
 }
 
-const player1 = createPlayer('Player One', 'x');
-const player2 = createPlayer('Player Two', 'o');
+const game = (function() {
+    let player1;
+    let player2;
+    let openingPlayer;
+    let currentPlayer;
+    let turnResult;
 
-const game = (function() {    
-    let currentPlayer = player1;
-
-    const endRound = () => {
-        console.log(`${player1.getName()}: ${player1.getScore()} | ${player2.getName()}: ${player2.getScore()}`);
-        
+    const start = (player1Name, player2Name) => {
+        player1 = createPlayer(player1Name, '\u{2715}');
+        player2 = createPlayer(player2Name, '\u{25EF}');
+        newRound();
+        turnResult = `${currentPlayer.getName()} (${currentPlayer.getMarker()}), it's your turn!`;
+    }
+   
+    const newRound = () => {        
+        openingPlayer = (openingPlayer === player1) ? player2 : player1;
+        currentPlayer = openingPlayer;
         board.clear();
-        currentPlayer = player1;
     }
 
-    const endGame = () => {
-        endRound();
-        player1.clearScore;
-        player2.clearScore;
+    const getTurnInfo = () => {
+        const name1 = player1.getName();
+        const name2 = player2.getName();
+        const score1 = player1.getScore();
+        const score2 = player2.getScore();
+        return { name1, name2, score1, score2, turnResult }
     }
 
     const checkWinner = () => {
         const patterns = ['012', '345', '678', '036', '147', '258', '048', '246'];
 
-        patterns.forEach((pattern) => {
+        for (const pattern of patterns) {
             const regex = new RegExp(`(?=.*${pattern.charAt(0)})(?=.*${pattern.charAt(1)})(?=.*${pattern.charAt(2)}).*`);
-            
-            if (regex.test(currentPlayer.getPositions())) {
+
+            if (regex.test(board.getPlayerPositions(currentPlayer))) {
                 currentPlayer.increaseScore();
-                console.log(`${currentPlayer.getName()} won!`);
-                endRound();
+                turnResult = `${currentPlayer.getName()} (${currentPlayer.getMarker()}) won!`;
+                return true;
             }
-        });
+        }
     }
 
     const checkDraw = () => {
-        if (!board.get().includes(undefined)) {
-            console.log('draw!');
-            endRound();
+        if (!board.get().includes('')) {
+            turnResult = 'Draw!';
+            return true;
         }
     }
-
+    
     const switchPlayer = () => {
         currentPlayer = (currentPlayer === player1) ? player2 : player1;
+        turnResult = `${currentPlayer.getName()} (${currentPlayer.getMarker()}), it's your turn!`;
     }
 
     const takeTurn = (position) => {
-        if (board.get()[position] === undefined) {
+        if (board.get()[position] === '') {
             board.markPosition(currentPlayer.getMarker(), position);
-            // displayControl.updateDisplay();
 
-            console.log(currentPlayer.getName());
-            console.log(currentPlayer.getPositions());
-            
-            checkWinner();
-            checkDraw();
-            switchPlayer();
-             
+            if (checkWinner() === true || checkDraw() === true) {
+                newRound();
+            } else {
+                switchPlayer();
+            }
+            // return getTurnInfo();
         }
     }
 
-    return { takeTurn, endGame };
+    return { start, getTurnInfo, takeTurn };
 })();
-
-game.takeTurn(4);
-game.takeTurn(6);
-game.takeTurn(0);
-game.takeTurn(7);
-game.takeTurn(8);
